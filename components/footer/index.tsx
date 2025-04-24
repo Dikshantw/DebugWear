906f87a104e2506a123cee9942da67b16c9e8918
Dikshant Wavhule <dikshantw11@gmail.com>
2025-04-24 18:37:24 +0000

    style(footer): apply padding to footer

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t">
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center p-5">
         {currentYear} DebugWear. All Rights Reserved
       </div>
     </footer>
