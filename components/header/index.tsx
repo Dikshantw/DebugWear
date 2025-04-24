65019389d2ec52dee9a7c5d02c1bf64abd19fe26
Dikshant Wavhule <dikshantw11@gmail.com>
2025-04-24 13:06:19 +0000

    feat(header): create initial header with navigation

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,5 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { ShoppingCart, UserIcon } from "lucide-react";
+
 const Header = () => {
-  return <>Header</>;
+  return (
+    <header className="w-full border-b">
+      <div className="flex justify-between items-center max-w-7xl lg:mx-auto p-5 md:px-10 w-full">
+        <div className="flex justify-start items-center">
+          <Link href={"/"} className="flex justify-start items-center">
+            <Image
+              src="/terminal.svg"
+              alt="DebugWear Logo"
+              height={48}
+              width={48}
+              priority={true}
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              DebugWear
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant={"ghost"}>
+            <Link href={"/cart"}>
+              <ShoppingCart /> Cart
+            </Link>
+          </Button>
+          <Button asChild variant={"ghost"}>
+            <Link href={"/sign-in"}>
+              <UserIcon /> Cart
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
