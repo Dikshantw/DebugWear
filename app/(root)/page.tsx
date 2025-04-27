ed69017efe9d8f21f18509c8d2159f868f80d72e
Dikshant Wavhule <dikshantw11@gmail.com>
2025-04-27 07:21:02 +0000

    feat(UI): display products from db with product card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
-const Home = () => {
-  return <div>Home</div>;
+import ProductList from "@/components/product/productList";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+const Home = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList data={latestProducts} title="New Arrivals" limit={4} />
+    </>
+  );
 };
 
 export default Home;
---
+++ components/product/productCard.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import Image from "next/image";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const ProductCard = ({ product }: { product: any }) => {
+  return (
+    <Card>
+      <CardHeader>
+        <Link href={`product/${product.slug}`}>
+          <Image
+            src={product.images[0]}
+            alt={product.name}
+            height={300}
+            width={300}
+            priority={true}
+          />
+        </Link>
+      </CardHeader>
+      <CardContent className="p-4 grid gap-4">
+        <Link href={`product/${product.slug}`}>
+          <h2 className="text-sm font-medium">{product.name}</h2>
+        </Link>
+        <div className="flex justify-between items-center gap-4">
+          <p>{product.rating} Stars</p>
+          {product.stock > 0 ? (
+            <p className="font-bold">₹{product.price}</p>
+          ) : (
+            <p className="text-destructive">Out of Stocks</p>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ProductCard;
